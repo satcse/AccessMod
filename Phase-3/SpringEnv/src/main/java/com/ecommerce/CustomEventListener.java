d6c9e37ff5e3e48ccf8ab392d28ee9146d7924a1
satcse <sathish.tipofeverest@gmail.com>
2021-06-12 22:40:50 +0530

    Phase 3- Session 1

---
+++ Phase-3/SpringEnv/src/main/java/com/ecommerce/CustomEventListener.java
@@ -0,0 +1,14 @@
+package com.ecommerce;
+
+import org.springframework.context.ApplicationListener;
+
+import com.ecommerce.beans.CustomEvent;
+
+public class CustomEventListener implements ApplicationListener<CustomEvent>{
+
+	public void onApplicationEvent(CustomEvent event) {
+		System.out.println(event.toString());
+		
+	}
+
+}
